988bf2fe17fbe2031a8c20f1ba11f832cc7be960
Abdurrahmankayahan <abdurrahmankyhn@hotmail.com>
2024-03-12 22:14:35 +0300

    .

--- lib/src/screens/home_screen/components/body.dart
+++ lib/src/screens/home_screen/components/body.dart
@@ -200,3 +200,98 @@ class Body extends StatelessWidget {
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:smart360/config/size_config.dart';
+// import 'package:smart360/service/weather_service.dart';
+// import 'package:smart360/src/models/weather_model.dart';
+
+// class WeatherContainer extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<List<WeatherModel>>(
+//       future: WeatherService().getWeatherData(),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return Text('GUNCELLENIYOR');
+//         } else if (snapshot.hasError) {
+//           return Text('${snapshot.error}');
+//         } else if (snapshot.hasData) {
+//           List<WeatherModel> _weathers = snapshot.data!;
+//           return _buildWeatherContainer(_weathers);
+//         } else {
+//           return CircularProgressIndicator();
+//         }
+//       },
+//     );
+//   }
+  
+
+
+  
+// @override
+// Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         Container(
+//           height:
+//               getProportionateScreenHeight(120), // Increased container height
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(15),
+//             color: const Color(0xFFFFFFFF),
+//           ),
+//           child: Padding(
+//             padding: EdgeInsets.symmetric(
+//               horizontal: getProportionateScreenWidth(10),
+//               vertical: getProportionateScreenHeight(
+//                   10), // Increased vertical padding
+//             ),
+//             child: Row(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: [
+//                 SizedBox(
+//                   width: getProportionateScreenWidth(90),
+//                 ),
+//                 Column(
+//                   crossAxisAlignment: CrossAxisAlignment.end,
+//                   children: [
+//                     Text(
+//                       '${_weathers[0].durum.toUpperCase()}',
+//                       style: TextStyle(fontSize: 14), // Adjusted text style
+//                     ),
+//                     Text(
+//                       '${_weathers[0].derece.toLowerCase()}°C',
+//                       style: TextStyle(fontSize: 14), // Adjusted text style
+//                     ),
+//                     SizedBox(
+//                       height: getProportionateScreenHeight(5),
+//                     ),
+//                     Text(
+//                       '${_weathers[0].gun.toUpperCase()}',
+//                       style: TextStyle(fontSize: 14), // Adjusted text style
+//                     ),
+//                     Text(
+//                       'Nem: ${_weathers[0].nem.toLowerCase()}',
+//                       style: TextStyle(fontSize: 14), // Adjusted text style
+//                     )
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//         Image.network(
+//           _weathers[0].ikon,
+//           height: getProportionateScreenHeight(110),
+//           width: getProportionateScreenWidth(140),
+//           fit: BoxFit.contain,
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+
