8be2879e634a6c04d23665ae1a291eb0c6c3c154
Abdurrahmankayahan <abdurrahmankyhn@hotmail.com>
2024-03-13 05:43:14 +0300

    hava durumu sorunları  ios problemleri çözüldü  tasarım değiştirildi

--- lib/src/screens/menu_page/components/weather_container.dart
+++ lib/src/screens/menu_page/components/weather_container.dart
@@ -7,12 +7,15 @@ class WeatherContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<WeatherModel>>(
-      future:  WeatherService().getWeatherData(),
-      builder: (BuildContext context, AsyncSnapshot<List<WeatherModel>> snapshot) {
+      future: WeatherService().getWeatherData(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<WeatherModel>> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text('GUNCELLENIYOR');
+              return _buildWeatherContainer(List.empty());
+          //return  Row(children:[Text('Güncelleniyor...' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20), ),CircularProgressIndicator(color: Colors.amber,)]);
         } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
+          return Text(
+              "Bir Sorun oluştu Daha sonra tekrar deneyin."); // Text('${snapshot.error}');
         } else if (snapshot.hasData) {
           List<WeatherModel> _weathers = snapshot.data!;
           return _buildWeatherContainer(_weathers);
@@ -25,7 +28,66 @@ class WeatherContainer extends StatelessWidget {
 
   Widget _buildWeatherContainer(List<WeatherModel> _weathers) {
     if (_weathers.isEmpty) {
-      return Container(); 
+    return Stack(
+      children: [
+        Container(
+          height: getProportionateScreenHeight(120),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: const Color(0xFFFFFFFF),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(5),
+              vertical: getProportionateScreenHeight(5),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child:  CircularProgressIndicator( color: Colors.amber,),
+                ),
+                Container(
+                  width: getProportionateScreenWidth(150),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Hava Durumu',
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold ,fontStyle: FontStyle.italic ),
+                      ),
+                      Divider(),
+                      Text(
+                        ' °C',
+                        style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),
+                      ),
+                      Text(
+                        'Nem:  %',
+                        style: TextStyle(fontSize: 15 ,fontWeight: FontWeight.bold ),
+                      ),
+                      Text(
+
+                        'Güncelleniyor..',
+                   
+                        style: TextStyle(fontSize: 20),
+                        overflow: TextOverflow.fade,
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+ 
+
+
+
+
     }
 
     return Stack(
@@ -38,51 +100,55 @@ class WeatherContainer extends StatelessWidget {
           ),
           child: Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: getProportionateScreenWidth(10),
-              vertical: getProportionateScreenHeight(10),
+              horizontal: getProportionateScreenWidth(5),
+              vertical: getProportionateScreenHeight(5),
             ),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                SizedBox(
-                  width: getProportionateScreenWidth(90),
+                Image.network(
+                  _weathers[0].ikon,
+                  height: getProportionateScreenHeight(100),
+                  width: getProportionateScreenWidth(80),
+                  fit: BoxFit.contain,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      '${_weathers[0].durum.toUpperCase()}',
-                      style: TextStyle(fontSize: 14),
-                    ),
-                    Text(
-                      '${_weathers[0].derece.toLowerCase()}°C',
-                      style: TextStyle(fontSize: 14),
-                    ),
-                    SizedBox(
-                      height: getProportionateScreenHeight(5),
-                    ),
-                    Text(
-                      '${_weathers[0].gun.toUpperCase()}',
-                      style: TextStyle(fontSize: 14),
-                    ),
-                    Text(
-                      'Nem: ${_weathers[0].nem.toLowerCase()}',
-                      style: TextStyle(fontSize: 14),
-                    )
-                  ],
-                ),
+                Container(
+                  width: getProportionateScreenWidth(100),
+                  child: Column(
+                    children: [
+                      Text(
+                        '${_weathers[0].gun.toUpperCase()}',
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold ,fontStyle: FontStyle.italic ),
+                      ),
+                      Divider(),
+                      Text(
+                        '${_weathers[0].derece.toLowerCase()}  °C',
+                        style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),
+                      ),
+                      Text(
+                        'Nem:  ${_weathers[0].nem.toLowerCase()} %',
+                        style: TextStyle(fontSize: 15 ,fontWeight: FontWeight.bold ),
+                      ),
+                      Text(
+
+                        '${_weathers[0].durum.toUpperCase()}',
+                        maxLines: 2,
+                        style: TextStyle(fontSize: 15),
+                        overflow: TextOverflow.fade,
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
         ),
-        Image.network(
-          _weathers[0].ikon,
-          height: getProportionateScreenHeight(110),
-          width: getProportionateScreenWidth(140),
-          fit: BoxFit.contain,
-        ),
       ],
     );
+ 
+ 
+ 
   }
 }
